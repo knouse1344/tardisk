8c8f3d241241bb2e283a259c71928143be99beb9
Bryan Knouse <bryan.knouse@gmail.com>
2015-07-16 16:15:35 -0400

    new imgs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -35,4 +35,32 @@ $(document).ready(function(){
 		}
 	});
 
+	function getScrollTop() {
+		if (typeof window.pageYOffset !== 'undefined' ) {
+		// Most browsers
+		return window.pageYOffset;
+		}
+
+		var d = document.documentElement;
+		if (d.clientHeight) {
+		// IE in standards mode
+		return d.scrollTop;
+		}
+
+		// IE in quirks mode
+		return document.body.scrollTop;
+	}
+	
+	window.onscroll = function() {
+		var box = document.getElementById('boxed'),
+		    scroll = getScrollTop();
+
+		if (scroll <= 28) {
+		  box.style.top = "30px";
+		}
+		else {
+		  box.style.top = (scroll + 2) + "px";
+		}
+	};
+
 });
