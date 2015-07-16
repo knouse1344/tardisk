589e20158e0a8c23f172c8c72aa6141694f8a4ae
Bryan Knouse <bryan.knouse@gmail.com>
2015-07-16 14:15:01 -0400

    smooth scroll

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,26 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+
+
+
+
+$(document).ready(function(){
+
+	////////////   Smooth Scrolling
+
+	$('a[href*=#]:not([href=#])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+		  var target = $(this.hash);
+		  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+		  if (target.length) {
+		    $('html,body').animate({
+		      scrollTop: target.offset().top - 50
+		    }, 1000);
+		    return false;
+		  }
+		}
+	});
+
+});
